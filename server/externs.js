56b142ad6184f64a3b7a4bfa887defc3e1544838
Koji Ashida <kojiashida@gmail.com>
2013-06-20 23:09:36 -0700

    Cleaned up server output so compile errors are more visible.

--- server/externs.js
+++ server/externs.js
@@ -4,8 +4,9 @@ var _ir2js = {};
  * @param {string} base_dir
  * @param {Array.<string>} in_files
  * @param {string} out_dir
+ * @param {boolean} silent
  */
-_ir2js.compileFiles = function(base_dir, in_files, out_dir) {};
+_ir2js.compileFiles = function(base_dir, in_files, out_dir, silent) {};
 
 /**
  * @param {string} basedir
